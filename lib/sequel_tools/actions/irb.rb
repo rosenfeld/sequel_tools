54be3b052fd42e2e5c866a4d966dd28930abb715
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2017-12-20 11:32:25 -0200

    Change the irb action implementation to make test pass with JRuby
    I don't understand why the test doesn't work with the commented code,
    even though the command itself works with JRuby, so, I decided to give
    it another implementation to make sure the tests are passing on JRuby
    too.

--- lib/sequel_tools/actions/irb.rb
+++ lib/sequel_tools/actions/irb.rb
@@ -5,8 +5,16 @@ require_relative '../actions_manager'
 class SequelTools::ActionsManager
   description = "Opens an IRB session started as 'sequel uri_to_database' (DB is available to irb)"
   Action.register :irb, description do |args, context|
-    config = context[:config]
-    uri = context[:uri_builder][config]
-    exec "bundle exec sequel #{uri}"
+    # This code does the job, but for some reason the test will timeout under JRuby
+    #config = context[:config]
+    #uri = context[:uri_builder][config]
+    #exec "bundle exec sequel #{uri}"
+
+    Action[:connect_db].run({}, context)
+    require 'irb'
+    ::DB = context[:db]
+    ARGV.clear
+    puts 'Your database is stored in DB...'
+    IRB.start
   end
 end
--- spec/actions/irb_spec.rb
+++ spec/actions/irb_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe 'irb action' do
   # TODO: why isn't this test working under JRuby? The rake task seems to work though
   # and the test for the shell action is pretty similar and works with JRuby...
-  it 'opens a sql console to the database', skip: (RUBY_PLATFORM == 'java') do
+  it 'opens a sql console to the database' do
     expected = /"sequel_tools_test"/
     expect{
       rake_exec_runner.wait_output 'db:irb', 'DB["select current_database()"].get', expected
