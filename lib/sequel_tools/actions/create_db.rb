04ea153c906f9e4a043c30b8ab9c478c5ff233d3
Alexey Matskevich <45947106+AlexeyMatskevich@users.noreply.github.com>
2020-08-15 19:00:49 +0300

    Update create_db.rb

--- lib/sequel_tools/actions/create_db.rb
+++ lib/sequel_tools/actions/create_db.rb
@@ -16,8 +16,6 @@ class SequelTools::ActionsManager
       context[:db].log_info "Created database '#{c[:dbname]}'"
     else
       puts 'Database already exists - aborting'
-      
-      exit 0
     end
   end
 end
